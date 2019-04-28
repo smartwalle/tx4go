91cf4761d78822b499bed09e90dbd9488da07692
smartwalle <smartwalle@gmail.com>
2019-04-28 09:32:04 +0800

    添加例子

---
+++ sample/server1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/server"
+	"github.com/micro/go-plugins/registry/etcdv3"
+	"github.com/smartwalle/log4go"
+	"github.com/smartwalle/pks"
+	pks_client "github.com/smartwalle/pks/plugins/client/pks_grpc"
+	pks_server "github.com/smartwalle/pks/plugins/server/pks_grpc"
+	"github.com/smartwalle/tx4go"
+	"time"
+)
+
+func main() {
+	var s = pks.New(
+		micro.Server(pks_server.NewServer(server.Address("192.168.1.99:8911"))),
+		micro.Client(pks_client.NewClient(client.PoolSize(10))),
+		micro.RegisterTTL(time.Second*10),
+		micro.RegisterInterval(time.Second*5),
+		micro.Registry(etcdv3.NewRegistry()),
+		micro.Name("tx-s1"),
+	)
+
+	tx4go.Init(s)
+
+	s.Handle("h1", func(ctx context.Context, req *pks.Request, rsp *pks.Response) error {
+		log4go.Infof("-----收到来自 %s 的请求-----\n", req.FromService())
+
+		tx, err := tx4go.Begin(ctx, func() {
+			log4go.Println("confirm")
+		}, func() {
+			log4go.Errorln("cancel")
+		})
+
+		if err != nil {
+			log4go.Infoln("tx error", err)
+			return nil
+		}
+
+		tx.Commit()
+
+		return nil
+	})
+
+	s.Run()
+}
---
+++ sample/server3.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"github.com/micro/go-micro"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/server"
+	"github.com/micro/go-plugins/registry/etcdv3"
+	"github.com/smartwalle/log4go"
+	"github.com/smartwalle/pks"
+	pks_client "github.com/smartwalle/pks/plugins/client/pks_grpc"
+	pks_server "github.com/smartwalle/pks/plugins/server/pks_grpc"
+	"github.com/smartwalle/tx4go"
+	"time"
+)
+
+func main() {
+	var s = pks.New(
+		micro.Server(pks_server.NewServer(server.Address("192.168.1.99:8931"))),
+		micro.Client(pks_client.NewClient(client.PoolSize(10))),
+		micro.RegisterTTL(time.Second*10),
+		micro.RegisterInterval(time.Second*5),
+		micro.Registry(etcdv3.NewRegistry()),
+		micro.Name("tx-s3"),
+	)
+
+	tx4go.Init(s)
+
+	time.AfterFunc(time.Second*2, func() {
+		for i := 0; i < 1; i++ {
+
+			var ctx = context.Background()
+
+			var tx, _ = tx4go.Begin(ctx, func() {
+				log4go.Println("confirm")
+			}, func() {
+				log4go.Errorln("cancel")
+			})
+
+			s.Request(tx.Context(), "tx-s2", "h2", nil, nil)
+
+			if i%2 == 0 {
+				tx.Commit()
+			} else {
+				tx.Rollback()
+			}
+			log4go.Println("done")
+
+		}
+
+	})
+
+	s.Run()
+}
